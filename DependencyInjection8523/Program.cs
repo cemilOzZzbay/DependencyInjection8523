1624ff9b251e5af815ac368cf22529dd76a3f132
cemil özbay <cemil_ozbay@hotmail.com>
2022-01-26 00:16:05 +0300

    Add project files.

---
+++ DependencyInjection8523/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection8523
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SurucuBase surucu1 = new Surucu()
+            {
+                Isim="Çağıl"
+            };
+            Araba araba = new Araba(surucu1);
+            araba.Sur();
+
+            SurucuBase surucu2 = new Surucu() 
+            { 
+                Isim="Simge"
+            };
+            Araba simgeninArabası = new Araba(surucu2);
+            simgeninArabası.Sur();
+
+            Console.ReadLine();
+        }
+    }
+    class Araba
+    {
+        private readonly SurucuBase _surucu;
+        
+        public Araba(SurucuBase surucu)
+        {
+            _surucu = surucu;
+        }
+        public void Sur()
+        {
+            Console.WriteLine("Araba: " + _surucu.Isim + " tarafından sürülüyor...");
+        }
+            
+    }
+    abstract class SurucuBase // ISurucuBase
+    {
+        public string Isim { get; set; }
+    }
+    class Surucu : SurucuBase
+    {
+        
+    }
+}
---
+++ DependencyInjectionDetails/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionDetails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger1 = new FileLogger();
+            ProductManager manager1 = new ProductManager(logger1);
+            manager1.Add();
+
+            ILogger logger2 = new DatabaseLogger();
+            manager1 = new ProductManager(logger2);
+            manager1.Add();
+
+            CustomerManger customerManager = new CustomerManger();
+            customerManager.Add(new DatabaseLogger());
+
+            CategoryManager categoryManager = new CategoryManager() 
+            { 
+                Logger=new FileLogger()
+            };
+            categoryManager.Add();
+            
+            Console.ReadLine();
+        }
+    }
+    interface ILogger
+    {
+        void Log();
+    }
+    class FileLogger:ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file");
+        }
+    }
+    class DatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to database");
+        }
+    }
+    class ProductManager
+    {
+        private readonly ILogger _logger;
+        
+        public ProductManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Add()
+        {
+            _logger.Log();
+            Console.WriteLine("Product added");
+        }
+    }
+    class CustomerManger
+    {
+        public void Add(ILogger logger)
+        {
+            logger.Log();
+            Console.WriteLine("Customer added");
+        }
+    }
+    class CategoryManager
+    {
+        public ILogger Logger { get; set; }
+
+        public void Add()
+        {
+            Logger.Log();
+            Console.WriteLine("Category added");
+        }
+    }
+}
